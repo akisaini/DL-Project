1596d4e4184c82c08199de0ed364c90b43f36f3d
Adhithya Kiran <76047988+adhithyakiran@users.noreply.github.com>
2023-04-22 17:01:55 -0400

    LSTM model

---
+++ lstm_adhi.py
@@ -0,0 +1,351 @@
+import pandas as pd
+import numpy as np
+
+import os
+import sys
+import random
+
+# librosa is a Python library for analyzing audio and music.
+import librosa
+import librosa.display
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.metrics import confusion_matrix, classification_report
+from sklearn.model_selection import train_test_split
+
+# to play the audio files
+from IPython.display import Audio
+
+
+from tensorflow.keras import backend as K
+import tensorflow as tf
+tf.random.set_seed(30)
+
+from keras.models import Sequential, Model
+from keras.layers import Dense, GRU, LSTM, Flatten, TimeDistributed, Flatten, BatchNormalization, Activation
+from keras.layers.convolutional import Conv3D, MaxPooling3D, Conv2D, MaxPooling2D, Conv1D, MaxPooling1D
+#from keras.layers.recurrent import LSTM
+from keras.callbacks import ModelCheckpoint, ReduceLROnPlateau, EarlyStopping
+from keras import optimizers
+from keras.optimizers import adam_v2
+from keras.layers import Dropout
+import os
+import warnings
+if not sys.warnoptions:
+    warnings.simplefilter("ignore")
+warnings.filterwarnings("ignore", category=DeprecationWarning)
+
+cwd = os.getcwd()
+Ravdess = cwd + os.path.sep + 'ravdess' + os.path.sep
+
+ravdess_directory_list = os.listdir(Ravdess)
+file_emotion = []
+file_statement = []
+file_path = []
+for dir in ravdess_directory_list:
+    # as their are 20 different actors in our previous directory we need to extract files for each actor.
+    actor = os.listdir(Ravdess + dir)
+    for file in actor:
+        part = file.split('.')[0]
+        part = part.split('-')
+        # third part in each file represents the emotion associated to that auido.
+        file_emotion.append(int(part[2]))
+        file_statement.append(int(part[4]))
+        file_path.append(Ravdess + dir + '/' + file)
+
+# dataframe for emotion of files
+# emotion_df = pd.DataFrame(file_emotion, columns=['Emotions'])
+
+# dataframe for path of files.
+# path_df = pd.DataFrame(file_path, columns=['Path'])
+# Ravdess_df = pd.concat([emotion_df, path_df], axis=1)
+Ravdess_df = pd.DataFrame({"Emotions": file_emotion, "Statement": file_statement, "Path": file_path})
+
+# changing integers to actual emotions.
+Ravdess_df.Emotions.replace(
+    {1: 'neutral', 2: 'calm', 3: 'happy', 4: 'sad', 5: 'angry', 6: 'fear', 7: 'disgust', 8: 'surprise'}, inplace=True)
+print(Ravdess_df.head())
+
+
+print(Ravdess_df.shape)
+
+
+Ravdess_df.groupby(['Emotions','Statement']).size().unstack()
+
+
+# creating a copy
+data_path = Ravdess_df[['Emotions','Path']].copy()
+data_path.to_csv("data_path.csv",index=False)
+data_path.head()
+
+
+# plt.title('Count of Emotions', size=16)
+# sns.countplot(data_path.Emotions)
+# plt.ylabel('Count', size=12)
+# plt.xlabel('Emotions', size=12)
+# sns.despine(top=True, right=True, left=False, bottom=False)
+# plt.show()
+
+
+def create_waveplot(data, sr):
+    plt.figure(figsize=(10, 3))
+    plt.title('Waveplot for audio', size=15)
+    librosa.display.waveshow(data, sr=sr)
+    plt.show()
+
+def create_spectrogram(data, sr):
+    # stft function converts the data into short term fourier transform
+    X = librosa.stft(data)
+    Xdb = librosa.amplitude_to_db(abs(X))
+    plt.figure(figsize=(12, 3))
+    plt.title('Spectrogram for audio', size=15)
+    librosa.display.specshow(Xdb, sr=sr, x_axis='time', y_axis='hz')
+    #librosa.display.specshow(Xdb, sr=sr, x_axis='time', y_axis='log')
+    plt.colorbar()
+
+
+#path = "/kaggle/input/ravdess-emotional-speech-audio/Actor_01/03-01-01-01-01-01-01.wav"
+path = Ravdess + 'Actor_01' + os.path.sep + '03-01-01-01-01-01-01.wav'
+data, sampling_rate = librosa.load(path)
+create_waveplot(data, sampling_rate)
+Audio(path)
+
+emotion='fear'
+path = np.array(data_path.Path[data_path.Emotions==emotion])[1]
+data, sampling_rate = librosa.load(path)
+create_waveplot(data, sampling_rate)
+create_spectrogram(data, sampling_rate)
+Audio(path)
+
+
+emotion='angry'
+path = np.array(data_path.Path[data_path.Emotions==emotion])[1]
+data, sampling_rate = librosa.load(path)
+create_waveplot(data, sampling_rate)
+create_spectrogram(data, sampling_rate)
+Audio(path)
+
+emotion='sad'
+path = np.array(data_path.Path[data_path.Emotions==emotion])[1]
+data, sampling_rate = librosa.load(path)
+create_waveplot(data, sampling_rate)
+create_spectrogram(data, sampling_rate)
+Audio(path)
+
+
+emotion='happy'
+path = np.array(data_path.Path[data_path.Emotions==emotion])[1]
+data, sampling_rate = librosa.load(path)
+create_waveplot(data, sampling_rate)
+create_spectrogram(data, sampling_rate)
+Audio(path)
+
+
+def noise(data):
+    noise_amp = 0.035*np.random.uniform()*np.amax(data)
+    data = data + noise_amp*np.random.normal(size=data.shape[0])
+    return data
+
+def stretch(data, rate=0.85):
+    return librosa.effects.time_stretch(data, rate=rate)
+
+def shift(data):
+    shift_range = int(np.random.uniform(low=-5, high = 5)*1000)
+    return np.roll(data, shift_range)
+
+def pitch(data, sampling_rate, pitch_factor=0.7):
+    return librosa.effects.pitch_shift(data, sr=sampling_rate, n_steps=pitch_factor)
+
+# taking any example and checking for techniques.
+path = np.array(data_path.Path)[1]
+data, sample_rate = librosa.load(path)
+
+
+plt.figure(figsize=(14,4))
+librosa.display.waveshow(y=data, sr=sample_rate)
+Audio(path)
+
+x = noise(data)
+plt.figure(figsize=(14,4))
+librosa.display.waveshow(y=x, sr=sample_rate)
+Audio(x, rate=sample_rate)
+
+
+x = stretch(data)
+plt.figure(figsize=(14,4))
+librosa.display.waveshow(y=x, sr=sample_rate)
+Audio(x, rate=sample_rate)
+
+x = shift(data)
+plt.figure(figsize=(14,4))
+librosa.display.waveshow(y=x, sr=sample_rate)
+Audio(x, rate=sample_rate)
+
+x = pitch(data, sample_rate)
+plt.figure(figsize=(14,4))
+librosa.display.waveshow(y=x, sr=sample_rate)
+Audio(x, rate=sample_rate)
+
+
+def extract_features(data):
+    # MFCC
+    mfcc = librosa.feature.mfcc(y=data, sr=sample_rate)
+    result = mfcc
+    return result
+
+# funtion to transform audio
+def transform_audio(data, fns):
+    fn = random.choice(fns)
+    if fn == pitch:
+        fn_data = fn(data, sampling_rate)
+    elif fn == "None":
+        fn_data = data
+    elif fn in [noise, stretch]:
+        fn_data = fn(data)
+    else:
+        fn_data = data
+    return fn_data
+
+
+def get_features(path):
+    # duration and offset are used to take care of the no audio in start and the ending of each audio files as seen above.
+    data, sample_rate = librosa.load(path, duration=2.5, offset=0.6)
+
+
+    #randomly transforming audio data
+    fns = [noise, pitch, "None"]
+
+    fn1_data = transform_audio(data, fns)
+    fn2_data = transform_audio(fn1_data, fns)
+    res1 = extract_features(fn2_data)
+    result = [np.array(res1[:,:108])]
+
+    fn1_data = transform_audio(data, fns)
+    fn2_data = transform_audio(fn1_data, fns)
+    res2 = extract_features(fn2_data)[:,:108]
+    result.append(res2)#np.vstack((result, res2)) # stacking vertically
+
+    fn1_data = transform_audio(data, fns)
+    fn2_data = transform_audio(fn1_data, fns)
+    res3 = extract_features(fn2_data)[:,:108]
+    result.append(res3)#np.vstack((result, res3)) # stacking vertically
+
+    return result
+
+
+
+#path = "/kaggle/input/ravdess-emotional-speech-audio/Actor_01/03-01-01-01-01-01-01.wav"
+
+path = cwd + os.path.sep + 'ravdess/' + 'Actor_01/03-01-01-01-01-01-01.wav'
+
+data, sampling_rate = librosa.load(path, duration=2.5, offset=0.6)
+#create_waveplot(data, sampling_rate, emotion)
+#create_spectrogram(data, sampling_rate, emotion)
+Audio(path)
+xx1 = librosa.feature.zero_crossing_rate(data)
+stft = np.abs(librosa.stft(data))
+xx2 = librosa.feature.chroma_stft(S=stft, sr=sample_rate)
+xx3 = librosa.feature.mfcc(y=data, sr=sample_rate)
+xx4 = librosa.feature.rms(y=data)
+xx5 = librosa.feature.melspectrogram(y=data, sr=sample_rate)
+xx1.shape, xx2.shape, xx3.shape, xx4.shape, xx5.shape, np.append(xx1,xx2,axis=0).shape
+
+#xx = extract_features(data)
+#fns = [noise, stretch, pitch]
+#xx1 = extract_features(stretch(data))
+#xx.shape, xx1.shape
+#xx = get_features(path)
+#len(xx)
+
+X, Y = [], []
+for path, emotion in zip(data_path.Path.to_list(), data_path.Emotions.to_list()):
+    feature = get_features(path)
+    for ele in feature:
+        if ele.shape == (20, 108):
+            X.append(ele)
+            # appending emotion 3 times as we have made 3 augmentation techniques on each audio file.
+            Y.append(emotion)
+        else:
+            print(ele.shape)
+
+
+len(X), len(Y), data_path.Path.shape
+
+
+dfm = pd.DataFrame()
+xx = pd.Series(X)
+for i in range(20):
+    dfm[i] = [k[i] for k in xx]
+
+Features = dfm.copy()
+Features['labels'] = Y
+#Features.to_csv('features.csv', index=False)
+Features.head()
+
+
+X = np.array(X)
+Y = np.array(Y)
+X.shape, Y.shape
+
+# As this is a multiclass classification problem onehotencoding our Y.
+encoder = OneHotEncoder()
+Y = encoder.fit_transform(np.array(Y).reshape(-1,1)).toarray()
+
+
+x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state=21, shuffle=True)
+x_train.shape, y_train.shape, x_test.shape, y_test.shape
+
+
+x_train = np.expand_dims(x_train, axis=3)
+x_train = np.swapaxes(x_train, 1, 2)
+x_train = np.expand_dims(x_train, axis=3)
+
+x_test = np.expand_dims(x_test, axis=3)
+x_test = np.swapaxes(x_test, 1, 2)
+x_test = np.expand_dims(x_test, axis=3)
+
+x_train.shape, y_train.shape, x_test.shape, y_test.shape
+
+
+input_shape = x_train.shape[1:]
+input_shape #(108, 162, 1, 1)
+
+
+model=Sequential()
+
+model.add(TimeDistributed(Conv1D(16, 3, padding='same', activation='relu'),
+                            input_shape=input_shape))
+model.add(TimeDistributed(BatchNormalization()))
+#model.add(TimeDistributed(MaxPooling2D((2,1))))
+
+model.add(TimeDistributed(Flatten()))
+model.add(LSTM(32))
+model.add(Dropout(0.2))
+
+model.add(Dense(units=32, activation='relu'))
+model.add(Dropout(0.2))
+
+model.add(Dense(units=8, activation='softmax'))
+
+model.summary()
+
+
+from keras.optimizers import adam_v2
+optimizer = adam_v2.Adam()
+model.compile(optimizer=optimizer,
+              loss = 'categorical_crossentropy',
+              metrics=['accuracy'])
+
+rlrp = ReduceLROnPlateau(monitor='val_loss', factor=0.5, verbose=1, patience=4, min_lr=0.0000001)
+
+epochs = 100
+history = model.fit(x_train, y_train, batch_size=128, epochs=epochs, validation_data=(x_test, y_test), callbacks=[rlrp])
+
+
+
+
+
+
+
